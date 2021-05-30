68cce4931097c1242edf2f75db5e693d43260c09
Mert Selimbeyoğlu <mertselimb@gmail.com>
2021-05-30 14:33:30 +0300

    add wallet/new route

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express"),
+  path = require("path"),
+  cookieParser = require("cookie-parser"),
+  logger = require("morgan");
 
-var indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index"),
+  walletRouter = require("./routes/wallet");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/wallet", walletRouter);
 
 module.exports = app;
