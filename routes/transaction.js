ea84e17f66231379e15046cb42ae8725756a325a
Mert Selimbeyoğlu <mertselimb@gmail.com>
2021-05-30 21:20:48 +0300

    end of the project

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,9 +3,14 @@ const express = require("express"),
   router = express.Router(),
   Web3 = require("web3");
 
-const url = "https://ropsten.infura.io/v3/82ca06dd24474153b6e8518663d1b5b1";
-const web3 = new Web3(url);
+// Leaving like this for easy testing. I know this isn't the best practice.
 
+// Etherium test network
+// const url = "https://ropsten.infura.io/v3/82ca06dd24474153b6e8518663d1b5b1";
+// Etherium main network
+const url = "https://mainnet.infura.io/v3/82ca06dd24474153b6e8518663d1b5b1";
+const web3 = new Web3(url);
+// Ropsten network wallets
 const address1 = "0x3d1ebf179472bfF3892521971a8c33738324e68e";
 const privateKey1 =
   "c99d72bf97b18d012e7d06fa671ad128db9b505c7b072ce3891180eb363a40cd";
@@ -14,79 +19,115 @@ const privateKey2 =
   "0x9e12131cd0e5a2c3bdf80eb86f428d02a1b4e4042a61c089ef632ef8faeffd4b";
 
 const unexpectedError = { error: "Unexpected error." };
+const formatError = { error: "Wrong format" };
 
+// Fetch transaction data on blocks
 router.post("/fetch", async (req, res, next) => {
   if (req.body.startBlock) {
     const endBlock = req.body.endBlock
       ? req.body.endBlock
       : await web3.eth.getBlockNumber();
 
-    web3.eth.getBlock(req.body.startBlock, (err1, startBlock) => {
-      if (err1) {
+    const transactions = [];
+    for (
+      let blockIndex = parseInt(req.body.startBlock);
+      blockIndex < parseInt(endBlock) + 1;
+      blockIndex++
+    ) {
+      const block = await web3.eth.getBlock(blockIndex);
+      console.log(
+        blockIndex,
+        "block.transactions.length",
+        block.transactions.length
+      );
+      if (block || block.number) {
+        for (const transactionHash of block.transactions) {
+          const transaction = await web3.eth.getTransaction(transactionHash);
+          if (parseFloat(transaction.value) > 0) {
+            transactions.push(formatTx(transaction, block.timestamp, "ETH"));
+          } else if (
+            transaction.to === "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+          ) {
+            transactions.push(formatTx(transaction, block.timestamp, "USDT"));
+          }
+        }
+      } else {
         console.error(err);
         res.json(unexpectedError);
-      } else {
-        web3.eth.getBlock(req.body.startBlock, async (err2, endBlock) => {
-          if (err2) {
-            console.console.error(err);
-            res.json(unexpectedError);
-          } else {
-            const transactions = [];
-            for (const transactionHash of startBlock.transactions) {
-              if (endBlock.transactions.includes(transactionHash)) {
-                const transaction = await web3.eth.getTransaction(
-                  transactionHash
-                );
-                transactions.push({ ...transaction });
-              }
-            }
-            res.json(transactions);
-          }
-        });
       }
-    });
+    }
+    res.json(transactions);
   } else {
-    res.json({ error: "Wrong format" });
+    res.json(formatError);
   }
 });
 
+// This endpoint should accept transaction parameters,
+// build it, sign it, and broadcast it to the network.
 router.post("/send", async (req, res, next) => {
-  const sender = await web3.eth.accounts.privateKeyToAccount(
-    req.body.privateKey
-  );
-  if (sender.address) {
-    web3.eth.getTransactionCount(sender.address, (err, count) => {
-      if (err) {
-        console.error(err);
-        res.json(unexpectedError);
-      } else {
-        const TxObject = {
-          nonce: web3.utils.toHex(count),
-          to: req.body.address,
-          value: web3.utils.toHex(req.body.amount),
-          gasLimit: web3.utils.toHex(21000),
-          gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
-        };
-        const tx = new Tx(TxObject, { chain: "ropsten" });
-        tx.sign(Buffer.from(req.body.privateKey, "hex"));
+  if (req.body.privateKey && req.body.address && req.body.amount) {
+    const sender = await web3.eth.accounts.privateKeyToAccount(
+      req.body.privateKey
+    );
+    const addressCheck = await web3.utils.isAddress(req.body.address);
+    if (sender.address && addressCheck) {
+      web3.eth.getTransactionCount(sender.address, (err, count) => {
+        if (err) {
+          console.error(err);
+          res.json(unexpectedError);
+        } else {
+          const TxObject = {
+            nonce: web3.utils.toHex(count),
+            to: req.body.address,
+            value: web3.utils.toHex(req.body.amount),
+            gasLimit: web3.utils.toHex(21000),
+            gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
+          };
+          const tx = new Tx(TxObject, { chain: "ropsten" });
+          tx.sign(Buffer.from(req.body.privateKey, "hex"));
 
-        const serializedTransaction = tx.serialize();
-        const raw = "0x" + serializedTransaction.toString("hex");
+          const serializedTransaction = tx.serialize();
+          const raw = "0x" + serializedTransaction.toString("hex");
 
-        web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-          if (err) {
-            console.error(err);
-            res.json(unexpectedError);
-          } else {
-            res.json({ txHash });
-          }
-        });
-      }
-    });
+          web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+            if (err) {
+              console.error(err);
+              res.json(unexpectedError);
+            } else {
+              res.json({ txHash });
+            }
+          });
+        }
+      });
+    } else if (!addressCheck) {
+      res.json({ error: "Incorrect address" });
+    } else {
+      console.error(sender);
+      res.json(unexpectedError);
+    }
   } else {
-    console.error(sender);
-    res.json(unexpectedError);
+    res.json(formatError);
   }
 });
+
+// Get transaction data
+router.post("/", async (req, res, next) => {
+  if (req.body.transactionHash) {
+    res.json(await web3.eth.getTransaction(req.body.transactionHash));
+  } else {
+    res.json(formatError);
+  }
+});
+
+const formatTx = (transaction, timestamp, token) => {
+  return {
+    from: transaction.s,
+    to: transaction.r,
+    amount: transaction.value,
+    token: token,
+    timestamp: timestamp,
+    blockNumber: transaction.blockNumber,
+  };
+};
 
 module.exports = router;
--- routes/wallet.js
+++ routes/wallet.js
@@ -2,10 +2,15 @@ const express = require("express"),
   router = express.Router(),
   Web3 = require("web3");
 
-const url = "https://ropsten.infura.io/v3/82ca06dd24474153b6e8518663d1b5b1";
+// Leaving like this for easy testing. I know this isn't the best practice.
+
+// Etherium test network
+// const url = "https://ropsten.infura.io/v3/82ca06dd24474153b6e8518663d1b5b1";
+// Etherium main network
+const url = "https://mainnet.infura.io/v3/82ca06dd24474153b6e8518663d1b5b1";
 const web3 = new Web3(url);
 
-
+// This endpoint should generate an ethereum wallet ‘private key’ and ‘address’
 router.get("/new", async (req, res, next) => {
   const wallet = await web3.eth.accounts.create();
   res.json(wallet);
